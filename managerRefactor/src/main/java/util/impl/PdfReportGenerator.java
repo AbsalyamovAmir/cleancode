7ab2f868bc15cad75b576d0d03d669a546455a8a
Absalyamov Amir <98415161+AbsalyamovAmir@users.noreply.github.com>
2025-05-16 12:47:29 +0400

    Manager refactor (#5)
    В проекте необходимо найти нарушения принципов программирования, описанных в предыдущих лекциях,  и исправить их.
    Основные проблемы:
        Нарушение SRP (Single Responsibility Principle): Класс EmployeeManager выполняет слишком много обязанностей: управление сотрудниками, работа с БД, логирование, отправка уведомлений
        Нарушение OCP (Open-Closed Principle): Метод generateReport не закрыт для модификаций при добавлении новых типов отчетов
        Нарушение DIP (Dependency Inversion Principle): Прямая зависимость от конкретной реализации MySQLConnection
        Нарушение DRY (Don't Repeat Yourself): Множественные вложенные if-else в processEmployeeData
        Отсутствие инкапсуляции: Проверки валидности данных разбросаны по методу processEmployeeData

---
+++ managerRefactor/src/main/java/util/impl/PdfReportGenerator.java
@@ -0,0 +1,13 @@
+package util.impl;
+
+import util.inter.IReportGenerator;
+
+/**
+ * Пример реализации генератора отчетов
+ */
+public class PdfReportGenerator implements IReportGenerator {
+    @Override
+    public void generate() {
+        // Логика генерации PDF
+    }
+}
